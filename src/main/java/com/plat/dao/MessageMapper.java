28d4204602216bf88a9f8127699af376bcfc9978
twinkle-zp <295186137@qq.com>
2019-05-05 19:34:16 +0800

    111

---
+++ src/main/java/com/plat/dao/MessageMapper.java
@@ -0,0 +1,21 @@
+package com.plat.dao;
+
+import com.plat.entity.Message;
+
+import java.util.List;
+
+public interface MessageMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Message record);
+
+    int insertSelective(Message record);
+
+    Message selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Message record);
+
+    int updateByPrimaryKey(Message record);
+
+    List<Message> findList(Integer good_id, Integer user_id);
+}
---
+++ src/main/java/com/plat/entity/Message.java
@@ -0,0 +1,95 @@
+package com.plat.entity;
+
+import java.util.Date;
+
+public class Message {
+    private Integer id;
+
+    private Integer toId;
+
+    private Integer fromId;
+
+    private Integer goodId;
+
+    private String content;
+
+    private Date date;
+
+    private String goodName;
+
+    private String toName;
+
+    private String fromName;
+
+    public String getGoodName() {
+        return goodName;
+    }
+
+    public void setGoodName(String goodName) {
+        this.goodName = goodName;
+    }
+
+    public String getToName() {
+        return toName;
+    }
+
+    public void setToName(String toName) {
+        this.toName = toName;
+    }
+
+    public String getFromName() {
+        return fromName;
+    }
+
+    public void setFromName(String fromName) {
+        this.fromName = fromName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getToId() {
+        return toId;
+    }
+
+    public void setToId(Integer toId) {
+        this.toId = toId;
+    }
+
+    public Integer getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(Integer fromId) {
+        this.fromId = fromId;
+    }
+
+    public Integer getGoodId() {
+        return goodId;
+    }
+
+    public void setGoodId(Integer goodId) {
+        this.goodId = goodId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
