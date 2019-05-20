f97202903f0fe6fc79e5bfcbe935adcdedb8acb9
twinkle-zp <295186137@qq.com>
2019-05-20 21:23:03 +0800

    111

---
+++ src/main/java/com/plat/dao/ResourceMapper.java
@@ -0,0 +1,27 @@
+package com.plat.dao;
+
+import com.plat.entity.Resource;
+
+import java.util.List;
+
+public interface ResourceMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Resource record);
+
+    int insertSelective(Resource record);
+
+    Resource selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Resource record);
+
+    int updateByPrimaryKey(Resource record);
+
+    int getTotalCount();
+
+    List<Resource> findPage(Integer beginRows, Integer pageSize);
+
+    int getMyTotalCount(Integer uid);
+
+    List<Resource> findMyPage(Integer beginRows, Integer pageSize,Integer uid);
+}
---
+++ src/main/java/com/plat/entity/Resource.java
@@ -0,0 +1,123 @@
+package com.plat.entity;
+
+public class Resource {
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private String address;
+
+    private Integer state;
+
+    private Integer uid;
+
+    private Integer flag;
+
+    private Integer tid;
+
+    private Integer num;
+
+    private Integer likenum;
+
+    private String uname;
+
+    private String suffix;
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Integer flag) {
+        this.flag = flag;
+    }
+
+    public Integer getTid() {
+        return tid;
+    }
+
+    public void setTid(Integer tid) {
+        this.tid = tid;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getLikenum() {
+        return likenum;
+    }
+
+    public void setLikenum(Integer likenum) {
+        this.likenum = likenum;
+    }
+}
